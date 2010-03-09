bacc10cf37be11dc1390312f7cd5ddd1f37f6d05
totte <Unknown>
2010-03-09 09:01:07 +0000

    changed layout on attack, writing mail

--- src/states/risk_attack.py
+++ src/states/risk_attack.py
@@ -21,16 +21,30 @@ from base import signal
 _log = get_log (__name__)
 
 
-class RiskAttackState (GameSubstate):
+class RiskAttackState (GameSubstate, ui.widget.VBox):
 
     def do_setup (self, *a, **k):
         super (RiskAttackState, self).do_setup (*a, **k)
         game = self.game
 	self.dst = game.ui_attack.dst
 	self.src = game.ui_attack.src
+	self.attacker_number_of_dices = self.game.ui_attack.attacker_number_of_dices
+	self.defender_number_of_dices = self.game.ui_attack.defender_number_of_dices
+	self.used_attacker_number = self.game.ui_attack.used_attacker_number
+	self.defender_dice_list = self.game.ui_attack.defender_dice_list
+	self.attacker_dice_list = self.game.ui_attack.attacker_dice_list
+	self.dice_enabled = self.game.ui_attack.dice_enabled
 
 	game.ui_attack.on_retreat += lambda ev: self.on_retreat (self)
+	game.ui_attack.on_attack += lambda ev: self.on_attack (self)
 	self.risk_attack(self.src, self.dst)
+
+	self._dice_box_attacker = ui.widget.VBox(self)
+	self._dice_box_attacker.set_visible (True)
+
+
+	self._dice_box_defender = ui.widget.VBox(self)
+	self._dice_box_defender.set_visible (True)
 
 
     def do_release (self):
@@ -43,23 +57,127 @@ class RiskAttackState (GameSubstate):
         _log.debug ('Attacking from %s to %s.' %
                     (str (src.model), str (dst.model)))
 	self.game.ui_attack.dice_enable()
-	#self.risk_attack (src, dst)
     
-
-    def risk_attack (self, src, dst):
+    def on_attack(self, _):
+	
 	"""
-	When pressing a highlighted country for attack the attack menu
-	will be enabled.
-	"""	
-	self.game.ui_attack.dice_enable()
+	Takes the amount of numbers in the number_of_dices, random a dice number and shows 
+	them as buttonimages with the corresponding number.
+	"""
+
+        self.defender_dice_list = []
+	self.attacker_dice_list = []
+	self.used_attacker_number = 0
+	print self.game.ui_attack.defender_number_of_dices
+
+	for x in range(0, self.defender_number_of_dices):
+	    temp = random.randint(1,6)
+	    self.defender_dice_list.append(temp)
+	    
+	    self._dice = ui.widget.SmallButton (
+                self._dice_box_attacker,None, 'data/icon/dice'+str(temp)+'.png')
+
+	for x in range(0, self.attacker_number_of_dices):
+
+	    temp = random.randint(1,6)
+	    self.attacker_dice_list.append(temp)
+
+	    self._dice = ui.widget.SmallButton (
+                self._dice_box_defender, None, 'data/icon/dice'+str(temp)+'.png')
+
+	self._dice_box_attacker.set_visible (True)
+	self._dice_box_defender.set_visible (True)
+	self.attacker_dice_list.sort()
+	self.defender_dice_list.sort()
+	self.lost_troop()
+	
+	
+
+    def lost_troop (self):
+	if len(self.attacker_dice_list) != 0 and len(self.defender_dice_list) != 0:
+	    if self.src.model.troops > 1 and self.dst.model.troops > 0:
+
+		if self.attacker_dice_list[0] == 0:
+		    self.dice_disable()
+		    return
+
+		elif self.defender_dice_list[0] == 0:
+		    return
+		    
+			
+	        elif self.attacker_dice_list[0] <= self.defender_dice_list[0]:
+		    """
+		    If attackers highest dice is less or equal to defenders highest he loses a unit
+		    and the number is popped from the defenders list aswell as the last number from the attackers list
+
+		    """
+		
+		    self.src.model.troops -= 1
+		    self.defender_dice_list.pop(0)
+		    self.defender_dice_list.append(0)
+		    self.attacker_dice_list.pop(len(self.attacker_dice_list)-1)
+
+	        elif self.attacker_dice_list[0] > self.defender_dice_list[0]:
+
+	   	    """
+		    If defenders highest dice is less than attackers highest he loses a unit
+		    and the number is popped from the attackers list
+ 
+		    """
+
+		    self.dst.model.troops -= 1
+		    self.src.model.troops -= 1
+		    self.src.model.used += 1
+		    self.used_attacker_number += 1
+		    self.attacker_dice_list.pop(0)
+		    self.attacker_dice_list.append(0)
+	        self.lost_troop()
+
+	    elif self.src.model.troops == 1:
+		"""
+		Attacker is out of attacking units
+		"""
+		return
+	        
+	    elif self.dst.model.troops == 0:
+		"""
+		Defender is out of units
+		"""
+		self.dst.model.owner = self.src.model.owner
+		self.dst.model.troops = len(self.attacker_dice_list)
+		self.src.model.used -= self.used_attacker_number
+		self.src.model.troops -= len(self.attacker_dice_list)-self.used_attacker_number
+		self.game.ui_attack.dice_disable()
+		self.manager.leave_state ()
+
+	elif len(self.attacker_dice_list) == 0:
+	    if self.src.model.troops == 1:
+		"""
+		Attacker is out of attacking units
+		"""
+		self.game.ui_attack.dice_disable()
+		self.manager.leave_state ()
+		
+	elif len(self.defender_dice_list) == 0:
+
+	    if self.dst.model.troops == 0:
+		"""
+		Defender is out of units
+		"""
+
+		self.dst.model.owner = self.src.model.owner
+		self.dst.model.troops = len(self.attacker_dice_list)
+		self.src.model.used -= self.used_attacker_number
+		self.src.model.troops -= len(self.attacker_dice_list)-self.used_attacker_number
+		self.game.ui_attack.dice_disable()
+		self.manager.leave_state ()
+		return
+	    else:
+	        self.src.model.troops -= len(self.attacker_dice_list)
+	        self.src.model.used += len(self.attacker_dice_list)-self.used_attacker_number
+		return
 
 
     def on_retreat(self, random):
 	self.game.ui_attack.dice_disable()
         self.manager.leave_state ()	# Exit risk_attack state
-
-    def on_troops_increase(self):
-	pass
-
-    def on_troops_decrease(self):
-	pass
--- src/ui/attack.py
+++ src/ui/attack.py
@@ -46,7 +46,7 @@ class AttackComponent (widget.HBox, object):
 
 	self.on_attack = signal.Signal ()
 	self.on_retreat = signal.Signal ()
-	
+	"""
 	self._dice_box_attacker = widget.VBox(self)
 	self._dice_box_attacker.set_position_rel (0.5, 0.5)
 	self._dice_box_attacker.set_center_rel (0.5, 0.5)
@@ -57,7 +57,7 @@ class AttackComponent (widget.HBox, object):
 	self._dice_box_defender.set_position_rel (0.5, 0.5)
 	self._dice_box_defender.set_center_rel (0.5, 0.5)
 	self._dice_box_defender.set_visible (self.dice_enabled)
-
+	"""
 	#buttons for increase/decrease troops
 
         self._attack_troops_increase = widget.Button (
@@ -91,133 +91,19 @@ class AttackComponent (widget.HBox, object):
 
         self._attack = widget.SmallButton (
             self._choice_box, 'Attack')
-        self._attack.on_click += lambda ev: self.attack (self)
+	self._attack.on_click += self.on_attack
+        #self._attack.on_click += lambda ev: self.attack (self)
 
         self._retreat = widget.SmallButton (
             self._choice_box, 'Retreat')
         self._retreat.on_click += self.on_retreat
 
 
-    def attack(self, _):
-
-	"""
-	Takes the amount of numbers in the number_of_dices, random a dice number and shows 
-	them as buttonimages with the corresponding number.
-	"""
-
-        self.defender_dice_list = []
-	self.attacker_dice_list = []
-	self.used_attacker_number = 0
-
-	for x in range(0, self.defender_number_of_dices):
-	    temp = random.randint(1,6)
-	    self.defender_dice_list.append(temp)
-	    
-	    self._dice = widget.SmallButton (
-                self._dice_box_attacker,None, 'data/icon/dice'+str(temp)+'.png')
-
-	for x in range(0, self.attacker_number_of_dices):
-
-	    temp = random.randint(1,6)
-	    self.attacker_dice_list.append(temp)
-
-	    self._dice = widget.SmallButton (
-                self._dice_box_defender, None, 'data/icon/dice'+str(temp)+'.png')
-
-	self._dice_box_attacker.set_visible (True)
-	self._dice_box_defender.set_visible (True)
-	self.attacker_dice_list.sort()
-	self.defender_dice_list.sort()
-	self.dice_enable()
-	self.lost_troop()
-	
-	
-
-    def lost_troop (self):
-	if len(self.attacker_dice_list) != 0 and len(self.defender_dice_list) != 0:
-	    if self.src.model.troops > 1 and self.dst.model.troops > 0:
-
-		if self.attacker_dice_list[0] == 0:
-		    self.dice_disable()
-		    return
-
-		elif self.defender_dice_list[0] == 0:
-		    return
-		    
-			
-	        elif self.attacker_dice_list[0] <= self.defender_dice_list[0]:
-		    """
-		    If attackers highest dice is less or equal to defenders highest he loses a unit
-		    and the number is popped from the defenders list aswell as the last number from the attackers list
-
-		    """
-		
-		    self.src.model.troops -= 1
-		    self.defender_dice_list.pop(0)
-		    self.defender_dice_list.append(0)
-		    self.attacker_dice_list.pop(len(self.attacker_dice_list)-1)
-
-	        elif self.attacker_dice_list[0] > self.defender_dice_list[0]:
-
-	   	    """
-		    If defenders highest dice is less than attackers highest he loses a unit
-		    and the number is popped from the attackers list
- 
-		    """
-
-		    self.dst.model.troops -= 1
-		    self.src.model.troops -= 1
-		    self.src.model.used += 1
-		    self.used_attacker_number += 1
-		    self.attacker_dice_list.pop(0)
-		    self.attacker_dice_list.append(0)
-	        self.lost_troop()
-
-	    elif self.src.model.troops == 1:
-		"""
-		Attacker is out of attacking units
-		"""
-		return
-	        
-	    elif self.dst.model.troops == 0:
-		"""
-		Defender is out of units
-		"""
-		self.dst.model.owner = self.src.model.owner
-		self.dst.model.troops = len(self.attacker_dice_list)
-		self.src.model.used -= self.used_attacker_number
-		self.src.model.troops -= len(self.attacker_dice_list)-self.used_attacker_number
-		self.dice_disable()
-
-	elif len(self.attacker_dice_list) == 0:
-	    if self.src.model.troops == 1:
-		"""
-		Attacker is out of attacking units
-		"""
-		self.dice_disable()
-		return
-		
-	elif len(self.defender_dice_list) == 0:
-
-	    if self.dst.model.troops == 0:
-		"""
-		Defender is out of units
-		"""
-
-		self.dst.model.owner = self.src.model.owner
-		self.dst.model.troops = len(self.attacker_dice_list)
-		self.src.model.used -= self.used_attacker_number
-		self.src.model.troops -= len(self.attacker_dice_list)-self.used_attacker_number		
-		self.dice_disable()
-		return
-	    else:
-	        self.src.model.troops -= len(self.attacker_dice_list)
-	        self.src.model.used += len(self.attacker_dice_list)-self.used_attacker_number
-		return
+    
 
     def attacker_troops_increase(self, _):
 	if self.attacker_number_of_dices < 3 and self.src.model.troops > 1:
-	    self.attacker_number_of_dices +=1
+	    self.attacker_number_of_dices += 1
 	    #self.src.model.troops -=1
 
     def attacker_troops_decrease(self, _):
@@ -227,7 +113,7 @@ class AttackComponent (widget.HBox, object):
 
     def defender_troops_increase(self, _):
 	if self.defender_number_of_dices < 2 and self.dst.model.troops > 1:
-	    self.defender_number_of_dices +=1
+	    self.defender_number_of_dices += 1
 	    #self.dst.model.troops -=1
 
     def defender_troops_decrease(self, _):
@@ -240,12 +126,8 @@ class AttackComponent (widget.HBox, object):
 	"""
 	Show the increase/decrease buttons when the destination area is chosen
 	"""
-	self._attacker_number_of_dices = 1
-	self._defender_number_of_dices = 1
 
 	self.dice_enabled = True
-        self._dice_box_attacker.set_visible (self.dice_enabled)
-	self._dice_box_defender.set_visible (self.dice_enabled)
 
 	self._choice_box.set_visible (self.dice_enabled)
 
@@ -260,8 +142,6 @@ class AttackComponent (widget.HBox, object):
 	Hide the increase/decrease buttons when the destination area is chosen
 	"""
 	self.dice_enabled = False
-        self._dice_box_attacker.set_visible (self.dice_enabled)
-	self._dice_box_defender.set_visible (self.dice_enabled)
 
 	self._choice_box.set_visible (self.dice_enabled)
 
@@ -269,11 +149,3 @@ class AttackComponent (widget.HBox, object):
 	self._attack_troops_decrease.set_visible (self.dice_enabled)
 	self._defense_troops_increase.set_visible (self.dice_enabled)
 	self._defense_troops_decrease.set_visible (self.dice_enabled)
-
-        #def on_retreat(self, random):
-
-    def enable_pass (self):
-        pass
-
-    def disable_pass (self):
-        pass
