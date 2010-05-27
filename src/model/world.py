189b49ab50af48e23dae9bdfebfa606eef612c9b
jbolivar <Unknown>
2010-05-27 11:47:59 +0000

    Changed license to GPLv3

--- src/model/world.py
+++ src/model/world.py
@@ -1,10 +1,28 @@
 #
-#  Copyright (C) 2009 The JAGSAT project team.
-#
-#  This software is in development and the distribution terms have not
-#  been decided yet. Therefore, its distribution outside the JAGSAT
-#  project team or the Project Course evalautors in Abo Akademy is
-#  completly forbidden without explicit permission of their authors.
+#  Copyright (C) 2009, 2010 JAGSAT Development Team (see below)
+#  
+#  This file is part of TF.
+#  
+#  TF is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#  
+#  TF is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  
+#  JAGSAT Development Team is:
+#    - Juan Pedro Bolivar Puente
+#    - Alberto Villegas Erce
+#    - Guillem Medina
+#    - Sarah Lindstrom
+#    - Aksel Junkkila
+#    - Thomas Forss
 #
 
 from base.changer import InstChanger
